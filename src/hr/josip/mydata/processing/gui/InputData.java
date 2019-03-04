b482e150ad71c2cffb74fa3806d516c30e066c54
Josip Bošnjak <jbosnjak@unipu.hr>
2019-03-04 18:44:51 +0100

    Sada se podaci postavljaju na vrijednosti prilikom inputa

--- src/hr/josip/mydata/processing/gui/InputData.java
+++ src/hr/josip/mydata/processing/gui/InputData.java
@@ -1,12 +1,14 @@
 package hr.josip.mydata.processing.gui;
 
+import hr.josip.mydata.processing.Date;
+import hr.josip.mydata.processing.PersonalId;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalDate;
-import java.util.ArrayList;
+
 import java.util.Calendar;
-import java.util.Date;
+
 
 public class InputData extends JFrame implements ActionListener {
     private JPanel panel1;
@@ -30,6 +32,7 @@ public class InputData extends JFrame implements ActionListener {
     private JRadioButton sexMradioButton1;
     private JRadioButton sexFRadioButton;
     private ButtonGroup sex;
+    private String sex1;
 
     public JPanel getPanel1() {
         for (int i=1;i<=31;i++){
@@ -71,7 +74,28 @@ public class InputData extends JFrame implements ActionListener {
         saveDataButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(idTextField.getText());
+                PersonalId id=new PersonalId();
+                id.setIdentityCardNumber(idTextField.getText());
+                id.setDateOfExpiry(new Date((int) dayecomboBox1.getSelectedItem(),(int) monthecomboBox1.getSelectedItem(),
+                        (int) yearecomboBox1.getSelectedItem()));
+                id.setSurname(surname.getText());
+                id.setName(name.getText());
+                id.setSex(sex1.charAt(0));
+                id.setCitizenship(citizenship.getText());
+                id.setDateOfBirth(new Date(
+                        (int) daybcomboBox1.getSelectedItem(),
+                        (int) monthbcomboBox1.getSelectedItem(),
+                        (int) yearbcomboBox1.getSelectedItem()
+                ));
+                id.setResidence(residence.getText());
+                id.setIssuedBy(issuedby.getText());
+                id.setDateOfIssue(new Date(
+                        (int) dayicomboBox1.getSelectedItem(),
+                        (int) monthicomboBox1.getSelectedItem(),
+                        (int) yearicomboBox1.getSelectedItem()
+                ));
+                id.setPersonalIdentificationNumber(pin.getText());
+                System.out.println(id);
                 System.out.println(dayecomboBox1.getSelectedItem());
 
 
@@ -81,12 +105,17 @@ public class InputData extends JFrame implements ActionListener {
         sexMradioButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                sex1=sexMradioButton1.getText();
+
                 System.out.println(sexMradioButton1.getText());
             }
         });
         sexFRadioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                sex1=sexFRadioButton.getText();
+
+
                 System.out.println(sexFRadioButton.getText());
             }
         });
